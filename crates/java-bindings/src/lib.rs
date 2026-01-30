2d52eeb330ec00b4e81f05803df3909f37058fb9
OfflineIntelligence <intelligencedevelopment.io@gmail.com>
2026-01-29 22:52:00 -0600

    Update binding crates to use version dependencies for publishing

--- crates/java-bindings/src/lib.rs
+++ crates/java-bindings/src/lib.rs
@@ -1,7 +1,7 @@
 //! Java bindings for the Offline Intelligence Library using JNI
 use jni::JNIEnv;
-use jni::objects::{JClass, JObject, JString, JValue};
-use jni::sys::{jstring, jlong, jobject, jboolean};
+use jni::objects::{JClass, JString, JValue};
+use jni::sys::{jstring, jlong, jobject};
 use std::sync::Arc;
 use tokio::runtime::Runtime;
 
@@ -40,21 +40,21 @@ pub extern "system" fn Java_com_offlineintelligence_OfflineIntelligence_newInsta
 /// JNI function to optimize context
 #[no_mangle]
 pub extern "system" fn Java_com_offlineintelligence_OfflineIntelligence_optimizeContext(
-    env: JNIEnv,
+    mut env: JNIEnv,
     _class: JClass,
     ptr: jlong,
     session_id: JString,
-    messages: jobject,
+    _messages: jobject,
     user_query: JString,
 ) -> jobject {
-    let instance = unsafe { &*(ptr as *const OfflineIntelligenceJNI) };
+    let _instance = unsafe { &*(ptr as *const OfflineIntelligenceJNI) };
     
     // Convert Java strings to Rust
-    let session_id: String = env.get_string(&session_id)
+    let _session_id: String = env.get_string(&session_id)
         .expect("Couldn't get session_id string!")
         .into();
     
-    let user_query_opt = if user_query.is_null() {
+    let _user_query_opt: Option<String> = if user_query.is_null() {
         None
     } else {
         Some(env.get_string(&user_query)
@@ -70,33 +70,34 @@ pub extern "system" fn Java_com_offlineintelligence_OfflineIntelligence_optimize
         .expect("Couldn't create OptimizationResult object");
     
     // Set fields (placeholder values)
-    env.set_field(result_object, "originalCount", "I", JValue::Int(0))
+    let result_ref = result_object.as_ref();
+    env.set_field(result_ref, "originalCount", "I", JValue::Int(0))
         .expect("Couldn't set originalCount field");
-    env.set_field(result_object, "optimizedCount", "I", JValue::Int(0))
+    env.set_field(result_ref, "optimizedCount", "I", JValue::Int(0))
         .expect("Couldn't set optimizedCount field");
-    env.set_field(result_object, "compressionRatio", "F", JValue::Float(0.0))
+    env.set_field(result_ref, "compressionRatio", "F", JValue::Float(0.0))
         .expect("Couldn't set compressionRatio field");
     
-    result_object
+    result_object.as_raw()
 }
 
 /// JNI function to search memory
 #[no_mangle]
 pub extern "system" fn Java_com_offlineintelligence_OfflineIntelligence_search(
-    env: JNIEnv,
+    mut env: JNIEnv,
     _class: JClass,
     ptr: jlong,
     query: JString,
     session_id: JString,
-    limit: i32,
+    _limit: i32,
 ) -> jobject {
-    let instance = unsafe { &*(ptr as *const OfflineIntelligenceJNI) };
+    let _instance = unsafe { &*(ptr as *const OfflineIntelligenceJNI) };
     
-    let query: String = env.get_string(&query)
+    let _query: String = env.get_string(&query)
         .expect("Couldn't get query string!")
         .into();
     
-    let session_id_opt = if session_id.is_null() {
+    let _session_id_opt: Option<String> = if session_id.is_null() {
         None
     } else {
         Some(env.get_string(&session_id)
@@ -112,14 +113,16 @@ pub extern "system" fn Java_com_offlineintelligence_OfflineIntelligence_search(
         .expect("Couldn't create SearchResult object");
     
     // Set fields (placeholder values)
-    env.set_field(result_object, "total", "I", JValue::Int(0))
+    let result_ref = result_object.as_ref();
+    env.set_field(result_ref, "total", "I", JValue::Int(0))
         .expect("Couldn't set total field");
-    env.set_field(result_object, "searchType", "Ljava/lang/String;", 
-        JValue::Object(*env.new_string("keyword")
-            .expect("Couldn't create string")))
+    let keyword_str = env.new_string("keyword")
+        .expect("Couldn't create string");
+    env.set_field(result_ref, "searchType", "Ljava/lang/String;", 
+        JValue::Object(keyword_str.as_ref()))
         .expect("Couldn't set searchType field");
     
-    result_object
+    result_object.as_raw()
 }
 
 /// JNI function to generate title
@@ -128,14 +131,14 @@ pub extern "system" fn Java_com_offlineintelligence_OfflineIntelligence_generate
     env: JNIEnv,
     _class: JClass,
     ptr: jlong,
-    messages: jobject,
+    _messages: jobject,
 ) -> jstring {
-    let instance = unsafe { &*(ptr as *const OfflineIntelligenceJNI) };
+    let _instance = unsafe { &*(ptr as *const OfflineIntelligenceJNI) };
     
     let title = env.new_string("Generated Title")
         .expect("Couldn't create title string");
     
-    title.into_inner()
+    title.as_raw()
 }
 
 /// JNI function to dispose instance
